87759160953e2fc06858a7f11845b3aea1c140a8
Michael Peterson <mtpeterson1948@gmail.com>
2024-08-21 15:28:47 -0700

    New localization symbols

--- MetricsLocales.lua
+++ MetricsLocales.lua
@@ -12,7 +12,6 @@ local ADDON_NAME, Metrics = ...
 ------------------------------------------------------------
 Metrics = Metrics or {}
 Metrics.Locales = {}
-
 local core = Metrics.MetricsCore
 
 ------------------------------------------------------------
@@ -25,67 +24,62 @@ local L = setmetatable({}, { __index = function(t, k)
 end })
 
 Metrics.Locales.L = L
-
--- usage example in another fileName
--- local L = Metrics.Locales.L
--- function ShowAddonMessage()
---     print(L["ADDON_LOADED_MESSAGE"])
--- end
--- ShowAddonMessage()
+local expansionName = core:getExpansionName()
+local version       = core:getVersion()
 
 local LOCALE = GetLocale()
 if LOCALE == "enUS" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MSG"] = string.format("%s %s (%s) loaded ", ADDON_NAME, core:getVersion(), core:getExpansionName())
-    L["INPUT_PARM_NIL"] = "ERROR: Input parameter nil. "
-    L["INVALID_TYPE"] = "ERROR: Input datatype invalid. "
+    L["VERSION"]            = version
+    L["EXPANSION_NAME"]     = expansionName
+    L["ADDON_LOADED_MESSAGE"]   = string.format("%s %s (%s) loaded ", ADDON_NAME, version, expansionName)
+    L["INPUT_PARM_NIL"]     = "ERROR: Input parameter nil. "
+    L["INVALID_TYPE"]       = "ERROR: Input datatype invalid. "
     L["ILLEGAL_OPERATION"]  = "ERROR: Operation is not supported. "
     L["UNEXPECTED_VALUE"]   = "ERROR: Value was unexpected. "
 elseif LOCALE == "frFR" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MESSAGE"] = string.format("%s (%s) chargé ", ADDON_NAME, core:getExpansionName())
+    L["VERSION"] = version
+    L["EXPANSION_NAME"] = expansionName
+    L["ADDON_LOADED_MESSAGE"] = string.format( "%s(%s) chargé ", ADDON_NAME, expansionName)
     L["INPUT_PARM_NIL"] = "ERREUR : Paramètre d'entrée nil. "
     L["INVALID_TYPE"] = "ERREUR : Type de données d'entrée invalide. "
     L["ILLEGAL_OPERATION"]  = "ERREUR : Opération non supportée. "
     L["UNEXPECTED_VALUE"]   = "ERREUR : Valeur inattendue. "
 elseif LOCALE == "deDE" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MESSAGE"] = string.format("%s (%s) geladen ", ADDON_NAME, core:getExpansionName())
+    L["VERSION"] = version
+    L["EXPANSION_NAME"] = expansionName
+    L["ADDON_LOADED_MESSAGE"] = string.format( "%s(%s) geladen ", ADDON_NAME, expansionName)
     L["INPUT_PARM_NIL"] = "FEHLER: Eingabeparameter nil. "
     L["INVALID_TYPE"] = "FEHLER: Ungültiger Eingabedatentyp. "
     L["ILLEGAL_OPERATION"]  = "FEHLER: Operation wird nicht unterstützt. "
     L["UNEXPECTED_VALUE"]   = "FEHLER: Unerwarteter Wert. "
 elseif LOCALE == "esES" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MESSAGE"] = string.format("%s (%s) cargado ", ADDON_NAME, core:getExpansionName())
+    L["VERSION"] = version
+    L["EXPANSION_NAME"] = expansionName
+    L["ADDON_LOADED_MESSAGE"] = string.format( "%s(%s) cargado ", ADDON_NAME, expansionName)
     L["INPUT_PARM_NIL"] = "ERROR: Parámetro de entrada nulo. "
     L["INVALID_TYPE"] = "ERROR: Tipo de dato de entrada inválido. "
     L["ILLEGAL_OPERATION"]  = "ERROR: Operación no soportada. "
     L["UNEXPECTED_VALUE"]   = "ERROR: Valor inesperado. "
 elseif LOCALE == "ruRU" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MESSAGE"] = string.format("%s (%s) загружен ", ADDON_NAME, core:getExpansionName())
+    L["VERSION"] = version
+    L["EXPANSION_NAME"] = expansionName
+    L["ADDON_LOADED_MESSAGE"] = string.format( "%s(%s) загружен ", ADDON_NAME, expansionName)
     L["INPUT_PARM_NIL"] = "ОШИБКА: Входной параметр nil. "
     L["INVALID_TYPE"] = "ОШИБКА: Неверный тип данных ввода. "
     L["ILLEGAL_OPERATION"]  = "ОШИБКА: Операция не поддерживается. "
     L["UNEXPECTED_VALUE"]   = "ОШИБКА: Неожиданное значение. "
 elseif LOCALE == "ptBR" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MESSAGE"] = string.format("%s (%s) carregado ", ADDON_NAME, core:getExpansionName())
+    L["VERSION"] = version
+    L["EXPANSION_NAME"] = expansionName
+    L["ADDON_LOADED_MESSAGE"] = string.format( "%s(%s) carregado ", ADDON_NAME, expansionName)
     L["INPUT_PARM_NIL"] = "ERRO: Parâmetro de entrada nulo. "
     L["INVALID_TYPE"] = "ERRO: Tipo de dado de entrada inválido. "
     L["ILLEGAL_OPERATION"]  = "ERRO: Operação não suportada. "
     L["UNEXPECTED_VALUE"]   = "ERRO: Valor inesperado. "
 elseif LOCALE == "itIT" then
-    L["VERSION"] = core:getVersion()
-    L["EXPANSION_NAME"] = core:getExpansionName()
-    L["ADDON_LOADED_MESSAGE"] = string.format("%s (%s) caricato ", ADDON_NAME, core:getExpansionName())
+    L["VERSION"] = version
+    L["EXPANSION_NAME"] = expansionName
+    L["ADDON_LOADED_MESSAGE"] = string.format( "%s(%s) caricato ", ADDON_NAME, expansionName)
     L["INPUT_PARM_NIL"] = "ERRORE: Parametro di input nullo. "
     L["INVALID_TYPE"] = "ERRORE: Tipo di dato di input non valido. "
     L["ILLEGAL_OPERATION"]  = "ERRORE: Operazione non supportata. "
